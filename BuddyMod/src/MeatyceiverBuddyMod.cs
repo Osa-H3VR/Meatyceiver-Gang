d93435ff20f1803f42878abb4b82c2d0ffbe8937
OsaPL <osa12pl@gmail.com>
2021-07-24 18:41:13 +0200

    Added buddymod

---
+++ BuddyMod/src/MeatyceiverBuddyMod.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Deli.Immediate;
+using Deli.Setup;
+using Deli.VFS;
+using Deli.VFS.Disk;
+using UnityEngine.SceneManagement;
+
+namespace Osa.MeatyceiverBuddyMod
+{
+    // DeliBehaviours are just MonoBehaviours that get added to a global game object when the game first starts.
+    public class MeatyceiverBuddyMod : DeliBehaviour
+    {
+        private TnhCharHook? _hooks;
+        
+        // All Deli properties can be accessed here, but don't use Unity's API until Awake.
+        public MeatyceiverBuddyMod()
+        {
+            // There is 1 log message in each of the methods here.
+            // Run this mod to see the order of execution logged to console.
+            Logger.LogInfo("BulletGrabber loading...");
+
+            // Hook to the setup stage (the first stage we can access)
+            // Do not forget! Awake is still ran before ANY event.
+            Stages.Setup += OnSetup;
+
+            // Check if the mod is on disk (as opposed to a zip file)
+            if (Resources is IDiskHandle resourcesOnDisk)
+            {
+                Logger.LogInfo($"The mod is on disk at: '{resourcesOnDisk.PathOnDisk}'");
+                
+                // Every time the scene changes, reload our resources to see if they changed.
+                SceneManager.activeSceneChanged += (x, p) => resourcesOnDisk.Refresh();
+            }
+        }
+
+        // Now you can use Unity's API
+        private void Awake()
+        {
+            LoadCfg();
+        }
+        
+        // The config system is identical to BepInEx, but here's a demo if you are unfamiliar.
+        // They are good with simple types (primitives and small structs), but for larger sets of data you may want to
+        // use an asset loader of some sort.
+        private void LoadCfg()
+        {
+            _hooks = new TnhCharHook(Logger, "meatyceiver", this);
+
+            SceneManager.activeSceneChanged += (x, p) =>
+            {
+                _hooks.UnhookChanges();
+            };
+        }
+        
+        // And now you can access much more of Deli
+        private void OnSetup(SetupStage stage)
+        {
+
+        }
+
+        private void OnDestroy()
+        {
+            _hooks?.Dispose();
+        }
+
+        private void Start()
+        {
+            Logger.LogInfo("BulletGrabber loaded.");
+        }
+    }
+}
---
+++ BuddyMod/src/TnhCharHook.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using BepInEx.Logging;
+using Deli.Setup;
+using FistVR;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using AutoMeaterHitZone = On.FistVR.AutoMeaterHitZone;
+using Damage = FistVR.Damage;
+using FVRPhysicalObject = FistVR.FVRPhysicalObject;
+using FVRViveHand = FistVR.FVRViveHand;
+using PyroSplodeyPack = On.FistVR.PyroSplodeyPack;
+using TNH_Manager = On.FistVR.TNH_Manager;
+
+namespace Osa.MeatyceiverBuddyMod
+{
+    public class TnhCharHook
+    {
+        private readonly ManualLogSource _manualLogSource;
+
+        private bool _enabled = false;
+
+        private readonly string _idFilter;
+        private readonly DeliBehaviour _behaviour;
+
+        public TnhCharHook(ManualLogSource manualLogSource, string idFilter, DeliBehaviour behaviour)
+        {
+            _manualLogSource = manualLogSource;
+            _idFilter = idFilter;
+            _behaviour = behaviour;
+            Hook();
+        }
+
+        public void Dispose()
+        {
+            Unhook();
+        }
+
+        public void Hook()
+        {
+            On.FistVR.TNH_Manager.SetPhase += OnTnh_ManagerOnSetPhase;
+        }
+
+        private void OnTnh_ManagerOnSetPhase(TNH_Manager.orig_SetPhase orig, FistVR.TNH_Manager self, TNH_Phase phase)
+        {
+            if (phase == TNH_Phase.Dead || phase == TNH_Phase.Completed)
+            {
+                UnhookChanges();
+            }
+            else
+            {
+                if (self.Phase == TNH_Phase.StartUp)
+                {
+                    if (self.C.TableID.Contains(_idFilter))
+                    {
+                        HookChanges();
+                    }
+                }
+            }
+
+            orig(self, phase);
+        }
+
+        public void Unhook()
+        {
+        }
+
+
+        public void UnhookChanges()
+        {
+            if (!_enabled)
+                return;
+            
+            _manualLogSource.LogWarning("UnHooking changes!");
+            On.FistVR.PyroSplodeyPack.Damage -= OnPyroSplodeyPackOnDamage;
+            On.FistVR.AutoMeaterHitZone.Damage -= OnAutoMeaterHitZoneOnDamage;
+            _enabled = false;
+        }
+
+        public void HookChanges()
+        {
+            if (_enabled)
+                return;
+
+            _manualLogSource.LogWarning("Hooking changes!");
+            On.FistVR.PyroSplodeyPack.Damage += OnPyroSplodeyPackOnDamage;
+            On.FistVR.AutoMeaterHitZone.Damage += OnAutoMeaterHitZoneOnDamage;
+            _enabled = true;
+        }
+
+        private void OnAutoMeaterHitZoneOnDamage(AutoMeaterHitZone.orig_Damage orig, FistVR.AutoMeaterHitZone self,
+            Damage damage)
+        {
+            //Turrets weakpoints are slightly to resistant
+            damage.Dam_TotalKinetic *= 2;
+            _manualLogSource.LogWarning("Yup, multiplied dmg to turret");
+            orig(self, damage);
+        }
+
+        private void OnPyroSplodeyPackOnDamage(PyroSplodeyPack.orig_Damage orig, FistVR.PyroSplodeyPack self,
+            Damage damage)
+        {
+            //Pyros tank is too hard to destroy
+            damage.Dam_TotalKinetic *= 3;
+            _manualLogSource.LogWarning("Yup, multiplied dmg to tank");
+            orig(self, damage);
+        }
+    }
+}
